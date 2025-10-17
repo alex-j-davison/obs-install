f0dc7a937928fccd9b2786486d752c4d8c1cddf9
Alex Davison <alexd@splunk.com>
2025-10-17 16:09:09 +0100

    sudo and microk8s

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 
 echo "###############################################################"
-echo "# Script: Step-3 Setup observability instance                 #"
+echo "# Script: Step-1 Setup observability instance                 #"
 echo "# Description: To create a observability instance.            #"
 echo "# Created date: 19/09/25                                      #"
 echo "# Arthur: Alex J Davison (alexdav@cisco.com)                  #"
@@ -12,49 +12,93 @@ echo "# Version: 0.0.1                                              #"
 echo "# Starting...                                                 #"
 echo "###############################################################"
 echo ""
-#echo "##########################"
-#echo "# Setups microk8s config #"
-#echo "##########################"
-#echo ""
-#echo "Step 1/5: Going home"
-#cd $HOME
-#echo "Step 2/5: Create folder"
-#mkdir .kube
-#echo "Step 3/5: Go in new folder"
-#cd .kube
-#echo "Step 4/5: Export config"
-#microk8s config > config
-#echo "Step 5/5: Go home"
-#cd $HOME
-#echo ""
+echo "#############"
+echo "# Update OD #"
+echo "#############"
+echo ""
+echo "Step 1/1: Updating OS"
+sudo apt-get update
+echo ""
+echo "####################"
+echo "# Install microk8s #"
+echo "####################"
+echo ""
+echo "Step 1/5: Install microk8s"
+sudo snap install microk8s --classic --channel=1.25/stable
+echo "Step 2/5: Enable DNS"
+sudo microk8s enable dns 
+echo "Step 3/5: Enable dashboard"
+sudo microk8s enable dashboard
+echo "Step 4/5: Enable storage"
+sudo microk8s enable storage
+echo "Step 5/5: Enable storage path"
+sudo microk8s enable storage hostpath-storage rbac ingress
+echo ""
+echo "#################"
+echo "# Creates alias #"
+echo "#################"
+echo ""
+echo "Step 1/3: Add helm alias"
+echo 'alias helm=microk8s.helm' | sudo tee -a  /etc/bash.bashrc
+echo "Step 2/3: Add kubectl alias"
+echo 'alias kubectl=microk8s.kubectl' | sudo tee -a  /etc/bash.bashrc
+echo "Step 3/3: Load alias"
+. /etc/bash.bashrc
+echo ""
+echo "############################"
+echo "# Permissions for microk8s #"
+echo "############################"
+echo ""
+echo "Step 1/3: Add group to user"
+echo "Run:- sudo usermod -a -G microk8s splunker"
+echo "Step 2/3: Change folder ownership"
+echo "Run:- sudo chown -f -R splunker ~/.kube"
+echo "Step 3/3: Create new group"
+echo "Run:- sudo newgrp microk8s"
+echo ""
+echo "##########################"
+echo "# Setups microk8s config #"
+echo "##########################"
+echo ""
+echo "Step 1/5: Going home"
+cd $HOME
+echo "Step 2/5: Create folder"
+mkdir .kube
+echo "Step 3/5: Go in new folder"
+cd .kube
+echo "Step 4/5: Export config"
+sudo microk8s config > config
+echo "Step 5/5: Go home"
+cd $HOME
+echo ""
 echo "#########################"
 echo "# Download kubeinvaders #"
 echo "#########################"
 echo ""
 echo "Step 1/1: Add kubeinvaders repo"
-shelm repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts/
+sudo microk8s helm repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts/
 echo ""
 echo "###########################"
 echo "# Download Otel collector #"
 echo "###########################"
 echo ""
 echo "Step 1/1: Adding otel collector"
-helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
+sudo microk8s helm repo add splunk-otel-collector-chart https://signalfx.github.io/splunk-otel-collector-chart
 echo ""
 echo "################"
 echo "# Update repos #"
 echo "################"
 echo "Step 1/1: Updating helm repos"
-helm repo update
+sudo microk8s helm repo update
 echo ""
 echo "#######################"
 echo "# Setups kubeinvaders #"
 echo "#######################"
 echo ""
 echo "Step 1/2: Creating namespace"
-kubectl create namespace kubeinvaders
+sudo microk8s kubectl create namespace kubeinvaders
 echo "Step 2/2: Inalling kubeinvaders"
-helm install --set-string config.target_namespace="namespace1\,namespace2" --set ingress.enabled=true --set ingress.hostName=kubeinvaders.local --set deployment.image.tag=latest -n kubeinvaders kubeinvaders kubeinvaders/kubeinvaders --set ingress.tls_enabled=true
+sudo microk8s helm install --set-string config.target_namespace="namespace1\,namespace2" --set ingress.enabled=true --set ingress.hostName=kubeinvaders.local --set deployment.image.tag=latest -n kubeinvaders kubeinvaders kubeinvaders/kubeinvaders --set ingress.tls_enabled=true
 echo ""
 echo "####################"
 echo "# Validate install #"
--- step4.sh
+++
@@ -1,6 +0,0 @@
-echo "#########################"
-echo "# Download kubeinvaders #"
-echo "#########################"
-echo ""
-echo "Step 1/1: Add kubeinvaders repo"
-sudo helm microk8s repo add kubeinvaders https://lucky-sideburn.github.io/helm-charts/
